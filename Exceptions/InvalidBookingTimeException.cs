54dce71b92021c69b6c591705664e00c60c1f674
Illia Piskurov <piskryov@gmail.com>
2026-08-17 10:14:23 +0300

    fix: call base constructor with message param

--- Exceptions/InvalidBookingTimeException.cs
+++ Exceptions/InvalidBookingTimeException.cs
@@ -1,5 +1,3 @@
 namespace ConferenceBookingApi.Exceptions;
 
-public class InvalidBookingTimeException(string message) : Exception
-{
-}
+public class InvalidBookingTimeException(string message) : Exception(message) {}
